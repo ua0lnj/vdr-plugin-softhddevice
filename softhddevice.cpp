64f1ef1b850d2aa067ec3b3822984d50d06ab452
root <ua0lnj@bk.ru>
2020-06-11 23:50:45 +1000

    Fix pip pes parser code.

--- softhddevice.cpp
+++ softhddevice.cpp
@@ -1941,6 +1941,13 @@ static void PipPesParse(const uint8_t * data, int size, int is_start)
     static int pes_size;
     static int pes_index;
 
+    if (!data) {
+	if (pes_buf) {
+	    free(pes_buf);
+	    pes_buf = NULL;
+	}
+	return;
+    }
     // FIXME: quick&dirty
 
     if (!pes_buf) {
@@ -1975,10 +1982,10 @@ static void PipPesParse(const uint8_t * data, int size, int is_start)
 	if (pes_index + size > pes_size) {
 	    pes_size = (pes_index + size) * 2;
 	}
-	pes_buf = (uint8_t *) realloc(pes_buf, pes_size);
-	if (!pes_buf) {			// out of memory, should never happen
+	void *res = (uint8_t *) realloc(pes_buf, pes_size);
+	if (!res) {			// out of memory, should never happen
 	    return;
-	}
+	} else pes_buf = (uint8_t *)res;
     }
     memcpy(pes_buf + pes_index, data, size);
     pes_index += size;
@@ -2064,6 +2071,7 @@ extern "C" void DelPip(void)
 {
     delete PipReceiver;
 
+    PipPesParse(NULL, 0, 0);
     PipReceiver = NULL;
     PipChannel = NULL;
 }
