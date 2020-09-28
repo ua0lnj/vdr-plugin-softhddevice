78f23fa8aed1d9edeeed969ea4e6ce4ee0dc7801
root <ua0lnj@bk.ru>
2020-09-28 22:19:47 +1000

    Fixed memory leak in CUVID grab code. Update Readme.

--- video.c
+++ video.c
@@ -12388,6 +12388,7 @@ static uint8_t *CuvidGrabOutputSurfaceLocked(int *ret_size, int *ret_width, int 
 	    }
 	}
 	glXMakeCurrent(XlibDisplay, None, NULL);
+	free(pixels);
 
 	Debug(3,"got grab data\n");
 
