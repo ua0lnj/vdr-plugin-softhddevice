4b6ce39dc2599150b69ed03e12fde46a836f91a4
root <ua0lnj@bk.ru>
2021-10-31 17:28:48 +1000

    More correct fix for subtitles.

--- openglosd.cpp
+++ openglosd.cpp
@@ -1996,7 +1996,7 @@ cOglOutputFb *cOglOsd::oFb = NULL;
 cOglOsd::cOglOsd(int Left, int Top, uint Level, std::shared_ptr<cOglThread> oglThread) : cOsd(Left, Top, Level) {
     this->oglThread = oglThread;
     bFb = NULL;
-    if (Level > 1)
+    if (Level == 10)
       isSubtitleOsd = true;
     else
       isSubtitleOsd = false;
