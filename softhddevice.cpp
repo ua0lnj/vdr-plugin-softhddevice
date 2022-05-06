78a500cf35258d66956360bff080d70ff079863f
root <ua0lnj@bk.ru>
2022-05-06 22:58:03 +1000

    Properly restoring window position after suspending in windowed mode. Version 1.6.0.

--- softhddevice.cpp
+++ softhddevice.cpp
@@ -64,7 +64,7 @@ extern "C"
     /// vdr-plugin version number.
     /// Makefile extracts the version number for generating the file name
     /// for the distribution archive.
-static const char *const VERSION = "1.5.1"
+static const char *const VERSION = "1.6.0"
 #ifdef GIT_REV
     "-GIT" GIT_REV
 #endif
