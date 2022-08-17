3ada16ad31225e0b0202995729e753430bbbec12
root <ua0lnj@bk.ru>
2022-08-17 23:51:13 +1000

    Small fix for vdpau.

--- video.c
+++ video.c
@@ -11203,6 +11203,7 @@ static void VdpauMixVideo(VdpauDecoder * decoder, int level)
 	}
 #endif
 	// FIXME: can use VDP_INVALID_HANDLE to support less surface on start
+	if (atomic_read(&decoder->SurfacesFilled) < 3) return;
 
 	if (VideoDeinterlaceSurfaces == 5) {
 	    past_n = 2;
