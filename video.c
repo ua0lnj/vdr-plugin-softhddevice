249b26ea713b93ea2c1b3384b07ba68f1c9946a1
root <ua0lnj@bk.ru>
2022-10-12 14:19:20 +1000

    Fix for vdpau software decoding. Enable 4:2:2 software decoding.

--- video.c
+++ video.c
@@ -10301,6 +10301,7 @@ static enum AVPixelFormat Vdpau_get_format(VdpauDecoder * decoder,
     decoder->Profile = VDP_INVALID_HANDLE;
     decoder->SurfacesNeeded = VIDEO_SURFACES_MAX + 2;
     decoder->PixFmt = AV_PIX_FMT_NONE;
+    video_ctx->draw_horiz_band = NULL;
 
     decoder->InputWidth = 0;
     decoder->InputHeight = 0;
@@ -10966,8 +10967,8 @@ static void VdpauRenderFrame(VdpauDecoder * decoder,
 	    case AV_PIX_FMT_YUV420P:
 	    case AV_PIX_FMT_YUVJ420P:	// some streams produce this
 	    case AV_PIX_FMT_YUV420P10LE:  // for softdecode of HEVC 10 Bit
-		break;
 	    case AV_PIX_FMT_YUV422P:
+		break;
 	    case AV_PIX_FMT_YUV444P:
 	    default:
 		Fatal(_("video/vdpau: pixel format %d not supported\n"),
